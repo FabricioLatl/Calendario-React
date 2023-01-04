42d4bea523f953de96c81545f0f49cfedb9ceb1b
Fabricio la Torre <39594850+FabricioLatl@users.noreply.github.com>
2023-01-04 11:25:52 -0500

    Update HoursAday.ts

--- src/util/HoursAday.ts
+++ src/util/HoursAday.ts
@@ -4,12 +4,12 @@ function create24HoursArray() {
   const hours: Array<tHours> = []
   for (let i = 0; i < 12; i++) {
     const hour = i
-    const item = { text: `오전 ${i === 0 ? 12 : i}`, hour }
+    const item = { text: `Morning ${i === 0 ? 12 : i}`, hour }
     hours.push(item)
   }
   for (let i = 0; i < 12; i++) {
     const hour = i === 0 ? 12 : i + 12
-    const item = { text: `오후 ${i === 0 ? 12 : i}`, hour }
+    const item = { text: `Afternoon ${i === 0 ? 12 : i}`, hour }
     hours.push(item)
   }
   return hours
